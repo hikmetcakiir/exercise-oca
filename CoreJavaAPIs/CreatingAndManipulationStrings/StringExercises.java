74995ec8c93f4897daffcac1afc70411fecea78c
HikmetCakir <hikmet.cakir@etiya.com>
2021-02-14 17:00:47 +0300

    StringExercises changed

--- CoreJavaAPIs/CreatingAndManipulationStrings/StringExercises.java
+++ CoreJavaAPIs/CreatingAndManipulationStrings/StringExercises.java
@@ -47,6 +47,49 @@ public class StringExercises{
 		System.out.println("MyCompany y index value from then 13's index is "+company.indexOf('y',13));
 		System.out.println("MyCompany om index value from then 4's index is "+company.indexOf("om",4));
 
+		String valueSix = "HelloWorld!";
+		System.out.println("substring value 1 is "+valueSix.substring(2,2)); //empty
+		System.out.println("substring value 1 is "+valueSix.substring(2,3)); 
+		System.out.println("substring value is "+valueSix.substring(1));
+		System.out.println("substring value is "+valueSix.substring(valueSix.length())); // empty
+		//System.out.println("substring value is "+valueSix.substring(-1,2)); //java.lang.StringIndexOutOfBoundsException: String index out of range: -1
+		//System.out.println("substring value is "+valueSix.substring(3,2)); //java.lang.StringIndexOutOfBoundsException: String index out of range: -1
+	
+		String valueSeven = "HelloWorld!";
+		System.out.println("valueSeven "+valueSeven);
+		valueSeven = "Hello";
+		System.out.println("valueSix "+valueSix);
+		System.out.println("valueSeven "+valueSeven);
 
+		String animal = "animal";
+		System.out.println("animal upper case form is "+animal.toUpperCase());
+		System.out.println("animal is "+animal);
+		System.out.println("animal lower case form is "+animal.toLowerCase());
+
+		System.out.println("ABC".equals("abc"));
+		System.out.println("ABC".equals("ABC"));
+
+		System.out.println("ABC".equalsIgnoreCase("Abc"));
+		System.out.println("ABC".equalsIgnoreCase("Abc "));
+
+		String compareValue = "Compare";
+		//System.out.println("Result "+compareValue.contains('o')); //Compile Error! char cannot be converted to CharSequence
+		System.out.println("Result "+compareValue.contains("om"));
+
+		String compareValueTwo = "CompareTwo";
+		//System.out.println("Result Two is "+compareValueTwo.startsWith('C')); //Compile Error! char cannot be converted to String
+		System.out.println("Result Two is "+compareValueTwo.startsWith("C"));
+		System.out.println("Result Two is "+compareValueTwo.endsWith("o"));
+		System.out.println("Result Two is "+compareValueTwo.endsWith("O"));
+
+		System.out.println("ABC".replace("A","a"));
+		System.out.println("ABC".replace('B','b'));
+		//System.out.println("ABC".replace("B",'b')); // no suitable method found for replace
+
+
+		String stringWithTrim = new String("\n\nVAL\tU E\t\t");
+		System.out.println("The value is : "+stringWithTrim.trim());
+
+		System.out.println("Result is "+stringWithTrim.trim().toLowerCase().replace("va","VA"));
 	}
 }
