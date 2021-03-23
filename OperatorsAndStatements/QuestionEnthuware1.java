5b655def861708f5e66e8ee4e8849dece57d8959
HikmetCakir <hikmet.cakir@etiya.com>
2021-03-24 00:48:56 +0300

    Solving question with Java Data Types and Using operators and decision constructs

---
+++ OperatorsAndStatements/QuestionEnthuware1.java
@@ -0,0 +1,15 @@
+
+ 
+ 
+
+public class QuestionEnthuware1 {
+    //final int value;
+	
+	public static void main(String... args) {
+		String local;
+		int n = 10;
+		if (10 > 9) local = "good";
+		System.out.println(local);
+	}
+   
+}
