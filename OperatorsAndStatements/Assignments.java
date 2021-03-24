207a764de07ca9901e0d7037f51956688d644d3c
HikmetCakir <hikmet.cakir@etiya.com>
2021-03-25 01:39:13 +0300

    Operators and statements question solving

---
+++ OperatorsAndStatements/Assignments.java
@@ -0,0 +1,31 @@
+class Assignments {	
+	int x = y();
+	public int y (){
+		return 2;
+	}
+	
+	public static void main(String... args) {
+		/*
+		long var1 = 0_100_267_760;
+		//long var2 = 0_._x_4_13;	
+		//long var3 = 0b_x10_BA_75;
+		//long var4 = 0xa10_AG_75;
+		long var5 = 0x1_0000_10;
+		//Boolean boolean2 =  new Boolean();
+		//Character ch = new Character();
+		//Integer n = new Integer();
+		Double d = new Double("1");
+		System.out.println(d);
+		double a = 0x10_B;
+		System.out.println(a);
+		short x = 3;
+		//short y = -x;
+		int z = (x < 4)?  int z = 4; x; : x++; 
+		*/
+
+		Long x = 4l;
+		Long y = 5L;
+		System.out.println(x + y);
+	}
+
+}
---
+++ OperatorsAndStatements/Test2.java
@@ -0,0 +1,15 @@
+
+
+public class Test2 {
+
+	public static void main(String... args) {
+		
+		boolean b1 = false;
+		boolean b2 = true;
+		b1 != b2;
+
+		System.out.println(b1!=b2);
+	}
+
+
+}
