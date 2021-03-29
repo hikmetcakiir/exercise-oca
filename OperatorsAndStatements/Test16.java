474fa5718cde4d21336f46bd03c8abdda33fed2b
HikmetCakir <hikmet.cakir@etiya.com>
2021-03-29 09:30:23 +0300

    Operators and statemets worked

--- OperatorsAndStatements/Test16.java
+++ OperatorsAndStatements/Test16.java
@@ -16,6 +16,15 @@ public class Test16 {
 		int tiger = ++lion *  5 / lion--;
 	 	System.out.println("tiger= " + tiger);
 	 	System.out.println("lion= " + lion);
+
+		Integer i = 5;
+		Integer j = 5;
+		
+		if(i == j) {
+			System.out.println("Equal");
+		} else {
+			System.out.println("Not Equal!");
+		}
 	}
 
 
